e25c180b915681571c04ae5bbf7fc4a7fe3f86c9
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2019-11-27 22:28:49 -0300

    Melhorando codigo

--- src/screens/Ocurrences/listPending.js
+++ src/screens/Ocurrences/listPending.js
@@ -8,98 +8,78 @@ import MainLayout from "../../layout/MainLayout";
 
 const { Content } = Layout;
 const { Title } = Typography;
+const { Column } = Table;
 
-export default function PendenciasPage() {
+const renderActions = (record, refresh) => {
+  return (
+    <span>
+      <Link to="/ocorrencia/">
+        <Icon
+          type="eye"
+          theme="twoTone"
+          twoToneColor="#5d7f28"
+          className={[
+            styles.iconAcaoLista, styles.iconOlhoAcaoLista, styles.iconDislike
+          ]}
+        />
+      </Link> 
+        <Popconfirm
+          title="Tem certeza que deseja aprovar a ocorrência?"
+          onConfirm={async() => {
+            try {
+              await mudaStatusOcorrencia(record.id, {status: "approved"});
+              notification['success']({
+                message: 'Ocorrência aprovada!',
+                description: 'A ocorrência foi aprovada com sucessso.'
+              });
+
+              refresh();
+
+            } catch(ex) {
+              notification['error']({
+                message: 'Ocorrência não aprovada com sucesso!',
+                description: 'Problemas ocorreram ao aprovar a ocorrência.'
+              });
+            }}
+                  }
+          onCancel={()=>{}}
+          okText="Sim"
+          cancelText="Não"
+        >
+          <Icon type = "like"  className = {styles.iconAcaoLista} />
+        </Popconfirm>
+
+        <Popconfirm
+          title="Tem certeza que deseja reprovar a ocorrência?"
+          onConfirm={async() => {
+            try {
+              await mudaStatusOcorrencia(record.id, {status: "rejected"});
+              notification['success']({
+                message: 'Ocorrência reprovada!',
+                description: 'A ocorrência foi reprovada com sucessso.'
+              });
+
+              refresh();
+
+            } catch(ex) {
+              notification['error']({
+                message: 'Ocorrência não reprovada!',
+                description: 'Problemas ocorreram ao reprovar a ocorrência.'
+              });
+            }}
+                  }
+          onCancel={()=>{}}
+          okText="Sim"
+          cancelText="Não"
+        >
+          <Icon type = "dislike"  className = {styles.iconDislike} />
+        </Popconfirm>
+      </span>
+  )
+}
+
+export default function PendingPage() {
   const [pendencias, setPendencias] = useState([]);
-
-  const columns = [
-    {
-      title: 'Descrição',
-      dataIndex: 'description',
-      onFilter: (value, record) => record.name.indexOf(value) === 0,
-      sorter: (a, b) => ("" + a.name).localeCompare(b.name),
-      defaultSortOrder: 'ascend',
-      sortDirections: ['ascend', 'descend'],
-    },
-    {
-      title: 'Categoria',
-      dataIndex: 'category_id',
-      onFilter: (value, record) => record.category_id.indexOf(value) === 0,
-      sorter: (a, b) => ("" + a.category_id).localeCompare(b.category_id),
-      sortDirections: ['ascend', 'descend'],
-      render: categoria => {
-        const categorias = {
-          solid_waste: 'Dejeito sólido',
-          dengue: 'Foco de dengue',
-          fire: 'Queimada',
-          waste: 'Esgoto',
-        }
-
-        return categorias[categoria];
-      }
-    },
-    {
-      title: 'Ações',
-      dataIndex: '',
-      key: 'x',
-      render: (text, record) => 
-      <span>
-        <Link to="/ocorrencia">
-        <Icon type = "eye" theme = "twoTone" twoToneColor = "#5d7f28" className = {[styles.iconAcaoLista, styles.iconOlhoAcaoLista, styles.iconDislike]} /></Link> 
-          <Popconfirm
-            title="Tem certeza que deseja aprovar a ocorrência?"
-            onConfirm={async() => {
-              try {
-                await mudaStatusOcorrencia(record.id, {status: "approved"});
-                notification['success']({
-                  message: 'Ocorrência aprovada!',
-                  description: 'A ocorrência foi aprovada com sucessso.'
-                });
-    
-                getPendingOccurences();
-    
-              } catch(ex) {
-                notification['error']({
-                  message: 'Ocorrência não aprovada com sucesso!',
-                  description: 'Problemas ocorreram ao aprovar a ocorrência.'
-                });
-              }}
-                    }
-            onCancel={()=>{}}
-            okText="Sim"
-            cancelText="Não"
-          >
-            <Icon type = "like"  className = {styles.iconAcaoLista} />
-          </Popconfirm>
-
-          <Popconfirm
-            title="Tem certeza que deseja reprovar a ocorrência?"
-            onConfirm={async() => {
-              try {
-                await mudaStatusOcorrencia(record.id, {status: "rejected"});
-                notification['success']({
-                  message: 'Ocorrência reprovada!',
-                  description: 'A ocorrência foi reprovada com sucessso.'
-                });
-    
-                getPendingOccurences();
-    
-              } catch(ex) {
-                notification['error']({
-                  message: 'Ocorrência não reprovada!',
-                  description: 'Problemas ocorreram ao reprovar a ocorrência.'
-                });
-              }}
-                    }
-            onCancel={()=>{}}
-            okText="Sim"
-            cancelText="Não"
-          >
-            <Icon type = "dislike"  className = {styles.iconDislike} />
-          </Popconfirm>
-        </span>,
-    },
-  ];
 
 // Colunas da tabela
 // Começam ordenadas crescente pelo campo Nome
@@ -117,7 +97,35 @@ export default function PendenciasPage() {
         <Title className = "titleLista" level={3}> Ocorrências - Pendências</Title>
         <Divider />
 
-        <Table rowKey="id" columns={columns} dataSource={pendencias} className={styles.tableOcorrencias} />
+        <Table rowKey="id" dataSource={pendencias} className={styles.tableOcorrencias}>
+          <Column
+            title='Descrição'
+            dataIndex='description'
+          />
+
+          <Column
+            title='Categoria'
+            dataIndex='category_id'
+            sorter={(a, b) => ("" + a.category_id).localeCompare(b.category_id)}
+            sortDirections={['ascend', 'descend']}
+            render={categoria => {
+              const categorias = {
+                solid_waste: 'Dejeito sólido',
+                dengue: 'Foco de dengue',
+                fire: 'Queimada',
+                waste: 'Esgoto',
+              }
+      
+              return categorias[categoria];
+            }}
+          />
+
+          <Column 
+            title='Ações'
+            key= 'x'
+            render={(record) => renderActions(record, getPendingOccurences)}
+          />
+        </Table>
     </Content>
   </MainLayout>
   );
